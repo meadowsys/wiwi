18bc418f22616c94c38e63fab0983f796e63240e
Meadowsys <blazeykirin@gmail.com>
2024-03-12 14:24:37 -0700

    copy over code from lazy-wrap@0.4.1

--- src/lazy_wrap.rs
+++ src/lazy_wrap.rs
@@ -1,26 +1,37 @@
 use ::parking_lot::{ Once, OnceState };
 use ::std::cell::UnsafeCell;
+use ::std::fmt::{ self, Debug, Display };
 use ::std::mem::ManuallyDrop;
 use ::std::ops::{ Deref, DerefMut };
-use ::std::panic::{ UnwindSafe, RefUnwindSafe };
+use ::std::panic::{ RefUnwindSafe, UnwindSafe };
+use ::std::ptr;
 
-pub struct LazyWrap<T, F = fn() -> T>
-where
-	F: FnOnce() -> T
-{
+union Data<T, F> {
+	init: ManuallyDrop<F>,
+	value: ManuallyDrop<T>
+}
+
+/// A lazily initialised data wrapper that initialises itself on first access
+pub struct LazyWrap<T, F = fn() -> T> {
 	data: UnsafeCell<Data<T, F>>,
 	once: Once
 }
 
-union Data<T, F> {
-	value: ManuallyDrop<T>,
-	init: ManuallyDrop<F>
+/// Returned by [`LazyWrap::into_inner`], containing the initialised value if
+/// its already initialised, or otherwise the initialisation function.
+pub enum LazyWrapState<T, F> {
+	/// Contains previously initialised value
+	Initialised(T),
+	/// Value is not initialised, contains initialisation function.
+	Uninitialised(F)
 }
 
 impl<T, F> LazyWrap<T, F>
 where
 	F: FnOnce() -> T
 {
+	/// Creates a new uninitialised instance that will be initialised with the
+	/// provided initialisation function.
 	#[inline]
 	pub const fn new(init: F) -> Self {
 		let init = ManuallyDrop::new(init);
@@ -29,13 +40,14 @@ where
 		Self { data, once }
 	}
 
+	/// Runs initialisation if the value is not initialised yet, and
+	/// blocks until it is complete.
+	///
+	/// Note: [`Deref`] and [`DerefMut`] automatically initialise if necessary.
 	#[inline]
 	pub fn ensure_initialised(this: &Self) {
 		this.once.call_once(|| {
-			// SAFETY: if this is executing, this must be the first time its
-			// executing, so its safe to take initialiser out of union
-
-			let data = unsafe { &mut *this.data.get() };
+			let data = unsafe { &mut (*this.data.get()) };
 			let init = unsafe { ManuallyDrop::take(&mut data.init) };
 			let value = init();
 			data.value = ManuallyDrop::new(value);
@@ -43,15 +55,51 @@ where
 	}
 
 	#[inline]
-	fn as_ref(this: &Self) -> &T {
+	fn ref_inner(this: &Self) -> &T {
 		Self::ensure_initialised(this);
 		unsafe { &(*this.data.get()).value }
 	}
 
 	#[inline]
-	fn as_mut(this: &mut Self) -> &mut T {
+	fn mut_inner(this: &mut Self) -> &mut T {
 		Self::ensure_initialised(this);
 		unsafe { &mut (*this.data.get()).value }
+	}
+
+	/// Returns true or false, depending on if the value is initialised.
+	#[inline]
+	pub fn is_initialised(this: &Self) -> bool {
+		use OnceState::*;
+		match this.once.state() {
+			New => { false }
+			Poisoned => { panic!("initialiser panicked") }
+			InProgress => {
+				this.once.call_once(|| {});
+				true
+			}
+			Done => { true }
+		}
+	}
+
+	pub fn into_inner(this: Self) -> LazyWrapState<T, F> {
+		let initialised = Self::is_initialised(&this);
+		let this = ManuallyDrop::new(this);
+		let data = unsafe { ptr::read(this.data.get()) };
+
+		if initialised {
+			let value = ManuallyDrop::into_inner(unsafe { data.value });
+			LazyWrapState::Initialised(value)
+		} else {
+			let init = ManuallyDrop::into_inner(unsafe { data.init });
+			LazyWrapState::Uninitialised(init)
+		}
+	}
+
+	pub fn into_inner_initialised(this: Self) -> T {
+		Self::ensure_initialised(&this);
+		let this = ManuallyDrop::new(this);
+		let data = unsafe { ptr::read(this.data.get()) };
+		ManuallyDrop::into_inner(unsafe { data.value })
 	}
 }
 
@@ -61,8 +109,9 @@ where
 {
 	type Target = T;
 	#[inline]
-	fn deref(&self) -> &T {
-		Self::as_ref(self)
+	fn deref(&self) -> &Self::Target {
+		// ensure_initialised is called by ref_inner
+		Self::ref_inner(self)
 	}
 }
 
@@ -71,8 +120,9 @@ where
 	F: FnOnce() -> T
 {
 	#[inline]
-	fn deref_mut(&mut self) -> &mut T {
-		Self::as_mut(self)
+	fn deref_mut(&mut self) -> &mut Self::Target {
+		// ensure_initialised is called by mut_inner
+		Self::mut_inner(self)
 	}
 }
 
@@ -84,6 +134,7 @@ where
 {
 	#[inline]
 	fn as_ref(&self) -> &U {
+		// ensure_initialised called by Deref
 		(**self).as_ref()
 	}
 }
@@ -96,23 +147,59 @@ where
 {
 	#[inline]
 	fn as_mut(&mut self) -> &mut U {
+		// ensure_initialised called by DerefMut
 		(**self).as_mut()
 	}
 }
 
-impl<T, F> Drop for LazyWrap<T, F>
+unsafe impl<T, F> Send for LazyWrap<T, F> where T: Send, F: Send {}
+unsafe impl<T, F> Sync for LazyWrap<T, F> where T: Sync, F: Send {}
+impl<T, F> UnwindSafe for LazyWrap<T, F> where T: UnwindSafe, F: UnwindSafe {}
+impl<T, F> RefUnwindSafe for LazyWrap<T, F> where T: RefUnwindSafe, F: UnwindSafe {}
+impl<T, F> Unpin for LazyWrap<T, F> where T: Unpin, F: Unpin {}
+
+impl<T, F> Debug for LazyWrap<T, F>
 where
+	T: Debug,
 	F: FnOnce() -> T
 {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		if Self::is_initialised(self) {
+			f.debug_struct("LazyWrap")
+				.field("initialised", &true)
+				.field("data", &**self)
+				.finish()
+		} else {
+			f.debug_struct("LazyWrap")
+				.field("initialised", &false)
+				.finish_non_exhaustive()
+		}
+	}
+}
+
+impl<T, F> Display for LazyWrap<T, F>
+where
+	T: Display,
+	F: FnOnce() -> T
+{
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		Display::fmt(&**self, f)
+	}
+}
+
+impl<T, F> Drop for LazyWrap<T, F> {
 	fn drop(&mut self) {
 		use OnceState::*;
-
 		match self.once.state() {
 			New => {
 				unsafe { ManuallyDrop::drop(&mut self.data.get_mut().init) }
 			}
 			Poisoned => {}
 			InProgress => {
+				// ???
+				// lets drop the thing once its done just in case
+				// this cannot happen though. if we're dropping, we're the last one with a reference.
+
 				self.once.call_once(|| {});
 				unsafe { ManuallyDrop::drop(&mut self.data.get_mut().value) }
 			}
@@ -122,9 +209,3 @@ where
 		}
 	}
 }
-
-unsafe impl<T, F> Send for LazyWrap<T, F> where T: Send, F: FnOnce() -> T + Send {}
-unsafe impl<T, F> Sync for LazyWrap<T, F> where T: Sync, F: FnOnce() -> T + Send {}
-
-impl<T, F> UnwindSafe for LazyWrap<T, F> where T: UnwindSafe, F: FnOnce() -> T + UnwindSafe {}
-impl<T, F> RefUnwindSafe for LazyWrap<T, F> where T: RefUnwindSafe, F: FnOnce() -> T + UnwindSafe {}
