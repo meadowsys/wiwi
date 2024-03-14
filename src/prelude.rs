e9c7c3c3ccf1ad35d9224bc3d19b538489e2b4a6
Meadowsys <blazeykirin@gmail.com>
2024-03-14 02:04:40 -0700

    add a prelude entries for debounce

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,11 +1,27 @@
 #[cfg(feature = "clock-timer")]
-pub use crate::clock_timer::{ ClockTimer, ClockTimerError, TimeDelta, Timelike };
+pub use crate::clock_timer::{
+	ClockTimer,
+	ClockTimerError,
+	TimeDelta,
+	Timelike
+};
+
+#[cfg(feature = "debounce")]
+pub use crate::debounce::{
+	debounce,
+	debounce_immediate,
+	debounce_with_rt,
+	debounce_immediate_with_rt
+};
 
 #[cfg(feature = "h")]
 pub use crate::h::h;
 
 #[cfg(feature = "lazy-wrap")]
-pub use crate::lazy_wrap::{ LazyWrap, LazyWrapState };
+pub use crate::lazy_wrap::{
+	LazyWrap,
+	LazyWrapState
+};
 
 #[cfg(feature = "string-pool")]
 pub use crate::string_pool::String;
