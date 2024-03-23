b9261245e6b3528f0b5ab99c77a8f96826a93948
Meadowsys <blazeykirin@gmail.com>
2024-03-22 22:04:58 -0700

    add hex to z85 benchmark
    oh wow its surprisingly slow in comparison to (both impls of) z85, ~247ms encode and ~380 decode for the same payload (while the z85 impls' times decreased by negligible amounts), this effort really felt worth it

--- benches/z85.rs
+++ benches/z85.rs
@@ -1,6 +1,5 @@
 use ::criterion::{ black_box, criterion_group, criterion_main, Criterion };
 use ::rand::{ Rng, thread_rng };
-use ::wiwi::z85::{ encode_z85, decode_z85 };
 
 fn benchmark(c: &mut Criterion) {
 	const FIFTY_MIB: usize = 50 * 1024 * 1024;
@@ -10,24 +9,33 @@ fn benchmark(c: &mut Criterion) {
 	rng.fill(&mut *bytes);
 	let bytes = &*bytes;
 
-	let encoded_wiwi = encode_z85(bytes);
+	let encoded_wiwi = ::wiwi::z85::encode_z85(bytes);
 	let encoded_wiwi = encoded_wiwi.as_bytes();
 
 	let encoded_z85 = ::z85::encode(bytes);
 	let encoded_z85 = encoded_z85.as_bytes();
 
+	let encoded_hex = ::hex::encode(bytes);
+	let encoded_hex = encoded_hex.as_bytes();
+
 	c
 		.bench_function("wiwi::z85::encode_z85 50MiB", |b| b.iter(|| {
-			encode_z85(black_box(bytes));
+			::wiwi::z85::encode_z85(black_box(bytes));
 		}))
 		.bench_function("wiwi::z85::decode_z85 50MiB", |b| b.iter(|| {
-			decode_z85(black_box(encoded_wiwi)).unwrap();
+			::wiwi::z85::decode_z85(black_box(encoded_wiwi)).unwrap();
 		}))
 		.bench_function("z85::encode 50MiB", |b| b.iter(|| {
 			::z85::encode(black_box(bytes));
 		}))
 		.bench_function("z85::decode 50MiB", |b| b.iter(|| {
 			::z85::decode(black_box(encoded_z85)).unwrap();
+		}))
+		.bench_function("hex::encode 50MiB", |b| b.iter(|| {
+			::hex::encode(black_box(bytes));
+		}))
+		.bench_function("hex::decode 50MiB", |b| b.iter(|| {
+			::hex::decode(black_box(encoded_hex)).unwrap();
 		}));
 }
 
