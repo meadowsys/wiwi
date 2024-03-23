3e2a975acd29351f37b1344a61bed48574c83193
Meadowsys <blazeykirin@gmail.com>
2024-03-23 16:10:07 -0700

    flipped order of bench (to see if it makes a diff, it doesn't), and let _

--- benches/z85.rs
+++ benches/z85.rs
@@ -19,23 +19,23 @@ fn benchmark(c: &mut Criterion) {
 	let encoded_hex = encoded_hex.as_bytes();
 
 	c
-		.bench_function("wiwi::z85::encode_z85 50MiB", |b| b.iter(|| {
-			::wiwi::z85::encode_z85(black_box(bytes));
-		}))
-		.bench_function("wiwi::z85::decode_z85 50MiB", |b| b.iter(|| {
-			::wiwi::z85::decode_z85(black_box(encoded_wiwi)).unwrap();
-		}))
-		.bench_function("z85::encode 50MiB", |b| b.iter(|| {
-			::z85::encode(black_box(bytes));
-		}))
-		.bench_function("z85::decode 50MiB", |b| b.iter(|| {
-			::z85::decode(black_box(encoded_z85)).unwrap();
+		.bench_function("hex::decode 50MiB", |b| b.iter(|| {
+			let _: Vec<u8> = ::hex::decode(black_box(encoded_hex)).unwrap();
 		}))
 		.bench_function("hex::encode 50MiB", |b| b.iter(|| {
-			::hex::encode(black_box(bytes));
+			let _: String = ::hex::encode(black_box(bytes));
 		}))
-		.bench_function("hex::decode 50MiB", |b| b.iter(|| {
-			::hex::decode(black_box(encoded_hex)).unwrap();
+		.bench_function("z85::decode 50MiB", |b| b.iter(|| {
+			let _: Vec<u8> = ::z85::decode(black_box(encoded_z85)).unwrap();
+		}))
+		.bench_function("z85::encode 50MiB", |b| b.iter(|| {
+			let _: String = ::z85::encode(black_box(bytes));
+		}))
+		.bench_function("wiwi::z85::decode_z85 50MiB", |b| b.iter(|| {
+			let _: Vec<u8> = ::wiwi::z85::decode_z85(black_box(encoded_wiwi)).unwrap();
+		}))
+		.bench_function("wiwi::z85::encode_z85 50MiB", |b| b.iter(|| {
+			let _: String = ::wiwi::z85::encode_z85(black_box(bytes));
 		}));
 }
 
