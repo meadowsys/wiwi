2438d8ca4b10f10ff5640ebf5caf0f37e70977ff
Meadowsys <blazeykirin@gmail.com>
2024-03-26 18:15:48 -0700

    i'll just commit what i have and put this off until later. lol
    reading spec kinda tiring, but yeah lol

--- src/ws/mod.rs
+++ src/ws/mod.rs
@@ -1,3 +1,25 @@
 //! Websocket implementation
 
 super::runtime_selection_compile_check!("ws");
+
+use ::tokio::net::TcpSocket;
+
+pub struct ClientBuilder {}
+pub struct Client {}
+
+pub struct ServerBuilder {}
+pub struct Server {}
+
+// pub struct Connection {}
+
+impl Client {
+	pub fn builder() -> ClientBuilder {
+		todo!()
+	}
+}
+
+impl Server {
+	pub fn builder() -> ServerBuilder {
+		todo!()
+	}
+}
