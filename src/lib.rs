79937bbde623d2eadd25263c70f652963fbc4b75
Meadow Liu <meadowsys@kiwin.gay>
2025-01-31 14:15:17 -0800

    classic me

--- src/lib.rs
+++ src/lib.rs
@@ -3,57 +3,57 @@
 	reason = "wip"
 )]
 
-pub use self::any::ExternAny;
-pub use self::bigint::ExternBigint;
-pub use self::boolean::ExternBoolean;
-pub use self::null::ExternNull;
-pub use self::number::ExternNumber;
-pub use self::object::ExternObject;
-pub use self::reflect::ExternReflect;
-pub use self::string::ExternString;
-pub use self::symbol::ExternSymbol;
-pub use self::undefined::ExternUndefined;
-pub use wasm_bindgen::prelude::wasm_bindgen;
+// pub use self::any::ExternAny;
+// pub use self::bigint::ExternBigint;
+// pub use self::boolean::ExternBoolean;
+// pub use self::null::ExternNull;
+// pub use self::number::ExternNumber;
+// pub use self::object::ExternObject;
+// pub use self::reflect::ExternReflect;
+// pub use self::string::ExternString;
+// pub use self::symbol::ExternSymbol;
+// pub use self::undefined::ExternUndefined;
+// pub use wasm_bindgen::prelude::wasm_bindgen;
 
-pub mod builder_api;
-pub mod extern_crates;
-mod sealed;
+// pub mod builder_api;
+// pub mod extern_crates;
+// mod sealed;
 
-mod any;
-mod bigint;
-mod boolean;
-mod null;
-mod number;
-mod object;
-mod reflect;
-mod string;
-mod symbol;
-mod undefined;
+// mod any;
+// mod bigint;
+// mod boolean;
+// mod null;
+// mod number;
+// mod object;
+// mod reflect;
+// mod string;
+// mod symbol;
+// mod undefined;
 
-#[allow(
-	dead_code,
-	unused_imports,
-	reason = "prelude"
-)]
-mod prelude_internal {
-	pub use crate::{ ExternAny, wasm_bindgen };
-	pub use crate::builder_api::*;
-	pub use crate::sealed::Sealed;
-	pub use std::marker::PhantomData;
-	pub use std::ops::{ Deref, DerefMut };
-	pub use wasm_bindgen::JsValue;
-}
+// #[allow(
+// 	dead_code,
+// 	unused_imports,
+// 	reason = "prelude"
+// )]
+// mod prelude_internal {
+// 	pub use crate::{ ExternAny, wasm_bindgen };
+// 	pub use crate::builder_api::*;
+// 	pub use crate::sealed::Sealed;
+// 	pub use std::marker::PhantomData;
+// 	pub use std::ops::{ Deref, DerefMut };
+// 	pub use wasm_bindgen::JsValue;
+// }
 
-#[cfg(test)]
-wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+// #[cfg(test)]
+// wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
-#[allow(
-	dead_code,
-	unused_imports,
-	reason = "prelude"
-)]
-#[cfg(test)]
-mod prelude_test {
-	pub use std::hint::black_box;
-	pub use wasm_bindgen_test::wasm_bindgen_test;
-}
+// #[allow(
+// 	dead_code,
+// 	unused_imports,
+// 	reason = "prelude"
+// )]
+// #[cfg(test)]
+// mod prelude_test {
+// 	pub use std::hint::black_box;
+// 	pub use wasm_bindgen_test::wasm_bindgen_test;
+// }
