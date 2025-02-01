f74a29b500712efa1272a60704e98f6e9fe4cefd
Meadow Liu <meadowsys@kiwin.gay>
2025-02-01 14:35:31 -0800

    conversions of references

--- src/any.rs
+++ src/any.rs
@@ -12,6 +12,12 @@ impl ExternAny {
 	}
 
 	#[inline(always)]
+	pub(crate) fn from_js_value_ref(value: &JsValue) -> &Self {
+		// SAFETY: ExternAny is repr(transparent) over JsValue
+		unsafe { &*(&raw const *value as *const ExternAny) }
+	}
+
+	#[inline(always)]
 	pub fn as_js_value(&self) -> &JsValue {
 		&self.inner
 	}
--- src/object.rs
+++ src/object.rs
@@ -6,10 +6,17 @@ pub struct ExternObject {
 }
 
 impl ExternObject {
-	#[inline]
+	#[inline(always)]
 	pub(crate) fn from_js_value_unchecked(value: JsValue) -> Self {
 		let inner = ExternAny::from_js_value(value);
 		Self { inner }
+	}
+
+	#[inline(always)]
+	pub(crate) fn from_js_value_ref_unchecked(value: &JsValue) -> &Self {
+		let value = ExternAny::from_js_value_ref(value);
+		// SAFETY: ExternObject is repr(transparent) over ExternAny
+		unsafe { &*(&raw const value as *const ExternObject) }
 	}
 }
 
