968b2f13553c6f0deb1119bafce88c63c474feb1
Meadow Liu <meadowsys@kiwin.gay>
2025-02-01 14:44:40 -0800

    more conversions and conversions of references,

--- src/object.rs
+++ src/object.rs
@@ -7,16 +7,26 @@ pub struct ExternObject {
 
 impl ExternObject {
 	#[inline(always)]
+	pub(crate) fn from_extern_any_unchecked(value: ExternAny) -> Self {
+		Self { inner: value }
+	}
+
+	#[inline(always)]
+	pub(crate) fn from_extern_any_ref_unchecked(value: &ExternAny) -> &Self {
+		// SAFETY: ExternObject is repr(transparent) over ExternAny
+		unsafe { &*(&raw const value as *const ExternObject) }
+	}
+
+	#[inline(always)]
 	pub(crate) fn from_js_value_unchecked(value: JsValue) -> Self {
-		let inner = ExternAny::from_js_value(value);
-		Self { inner }
+		let value = ExternAny::from_js_value(value);
+		Self::from_extern_any_unchecked(value)
 	}
 
 	#[inline(always)]
 	pub(crate) fn from_js_value_ref_unchecked(value: &JsValue) -> &Self {
 		let value = ExternAny::from_js_value_ref(value);
-		// SAFETY: ExternObject is repr(transparent) over ExternAny
-		unsafe { &*(&raw const value as *const ExternObject) }
+		Self::from_extern_any_ref_unchecked(value)
 	}
 }
 
