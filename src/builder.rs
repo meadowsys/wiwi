6d47710099462b071640783a047249ce4e52da7d
Meadow Liu <meadowsys@kiwin.gay>
2025-02-05 00:31:20 -0800

    write a builder checklist™ v2 based on reflect::set
    we test it next by implementing reflect.prevent_extensions() using it

--- src/builder.rs
+++ src/builder.rs
@@ -12,6 +12,19 @@
 // - impl blocks for finished ones with `call` or `build` fns
 // - impl block for builder fns, `change_state`, internal functions etc
 
+// Checklist™ v2
+// - use statements
+// - builder struct (with repr(transparent), `inner`, `__marker`)
+// - builder inner struct
+// - `gen_state!` invocation (generates state trait, state
+//   container struct, uninit type def, impl state for statecontainer)
+// - impl builder uninit
+// - impl blocks for finished ones with `execute` or `build` fns
+// - impl block for builder fns, `change_state`, internal functions etc
+// - target slot union definitions (will want `uninit`, likely will
+//   want `any`, and whatever other incompatible types in there), and
+//   associated impls (`Slot` and `SlotUnchecked` impls etc)
+
 pub use std::marker::PhantomData;
 
 pub struct Uninit {
