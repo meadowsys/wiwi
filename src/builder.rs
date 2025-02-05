0a474ab1db1e39697889659fea9805154945455e
Meadow Liu <meadowsys@kiwin.gay>
2025-02-05 03:45:58 -0800

    doc comments, safety comments, comments,

--- src/builder.rs
+++ src/builder.rs
@@ -27,10 +27,13 @@
 
 pub use std::marker::PhantomData;
 
+/// Marker struct for a field in the uninitialised state
 pub struct Uninit {
 	__private: ()
 }
 
+/// Marker struct for a field in the initialised state, optionally
+/// containing more state in the form of another type `T`
 pub struct Init<T = ()>
 where
 	T: ?Sized
@@ -43,17 +46,24 @@ where
 ///
 /// # Safety
 ///
-/// [`IS_INIT`](InitStatus::IS_INIT) and [`IS_UNINIT`](InitStatus::IS_UNINIT)
-/// must both be set correctly to acccurately represent the state of the field.
+/// [`IS_INIT`] and [`IS_UNINIT`] must both be set correctly to acccurately
+/// represent the state of the field. If you set [`IS_INIT`] correctly, there is
+/// a default implementation for [`IS_UNINIT`] which is just an inversion of
+/// [`IS_INIT`], and therefore always correct.
+///
+/// [`IS_INIT`]: InitStatus::IS_INIT
+/// [`IS_UNINIT`]: InitStatus::IS_UNINIT
 pub unsafe trait InitStatus {
 	const IS_INIT: bool;
 	const IS_UNINIT: bool = !Self::IS_INIT;
 }
 
+// SAFETY: `Uninit` represents uninitialised
 unsafe impl InitStatus for Uninit {
 	const IS_INIT: bool = false;
 }
 
+// SAFETY: `Init` represents initialised
 unsafe impl<T> InitStatus for Init<T>
 where
 	T: ?Sized
@@ -68,6 +78,7 @@ where
 /// Marker struct must actually represent an uninitialised state.
 pub unsafe trait IsUninit: InitStatus {}
 
+// SAFETY: `Uninit` represents uninitialised
 unsafe impl IsUninit for Uninit {}
 
 /// Marker trait for marker structs that represent initialised state
@@ -77,28 +88,95 @@ unsafe impl IsUninit for Uninit {}
 /// Marker struct must actually represent an initialised state.
 pub unsafe trait IsInit: InitStatus {}
 
+// SAFETY: `Init` represents initialised
 unsafe impl<T> IsInit for Init<T>
 where
 	T: ?Sized
 {}
 
+/// Types that can be used safely for slots of type `T`, just like
+/// [`SlotUnchecked`], but can only be implemented on types for which it is
+/// correct to use
+///
+/// For example, if we have an API that is expecting a string, we would only
+/// implement this trait for string types, but we can still implement
+/// [`SlotUnchecked`] for types that isn't guaranteed to be a string,
+/// like [`ExternAny`](crate::ExternAny).
+///
+/// To implement this trait, implement [`SlotUnchecked`] first, as that trait
+/// contains the actual implementation functionality. This trait is "only"
+/// a marker trait for those who implement [`SlotUnchecked`], and can guarantee
+/// that all values of the type are valid for this slot.
 pub unsafe trait Slot<T>
 where
 	Self: SlotUnchecked<T>
 {}
 
+/// Types that can be used for slots of type `T`, just like [`Slot`], but much,
+/// _much_ looser in restrictions in which types can implement this trait
+///
+/// See documentation on [`Slot`] for more details on these two traits.
 pub unsafe trait SlotUnchecked<T>
 where
 	Self: Sized
 {
+	/// Output type of reading from a slot previously written to (can be anything)
 	type Result: Sized;
+
+	/// Writes `self` into `slot`, doing as little work as needed
+	///
+	/// Work should be deferred to [`read`], if possible.
+	///
+	/// # Safety
+	///
+	/// Implementors must store a value into `slot`, that [`read`]
+	/// can read back later. [`read`] is allowed to rely on the fact that
+	/// something has been written.
+	///
+	/// It may be undesireable to call [`write`] twice on the same `slot`,
+	/// but even with that, there is no _strict_ requirement that callers only
+	/// call [`write`] once.
+	///
+	/// [`read`]: SlotUnchecked::read
+	/// [`write`]: SlotUnchecked::write
 	unsafe fn write(self, slot: &mut T);
+
+	/// Reads back what was written to `slot` in [`write`], then processes it
+	/// into the read output type as necessary
+	///
+	/// # Safety
+	///
+	/// Implementors can assume that [`write`] has been called on `slot` and a
+	/// value has been written as expected.
+	///
+	/// Callers must call [`write`] on this slot first, before passing it
+	/// to this function.
+	///
+	/// [`write`]: SlotUnchecked::write
 	unsafe fn read(slot: T) -> Self::Result;
+
+	/// Converts the read output type into a reference of type
+	/// [`&ExternAny`](crate::ExternAny)
+	///
+	/// This function is used so implementors can return non reference types.
+	/// Trait consumers would then take ownership of the provided output, and
+	/// use this function to get a reference type [`&ExternAny`](crate::ExternAny)
+	/// from it.
 	fn as_ref(result: &Self::Result) -> &crate::ExternAny;
 }
 
+/// [`PhantomData`] but invariant over `T`
+///
+/// To construct values of this type, you still must use the expression
+/// `PhantomData`, as Rust doesn't like using type definitions as unit struct
+/// constructors.
 pub(crate) type PhantomDataInvariant<T> = PhantomData<fn(T) -> T>;
 
+/// Provides convenience method
+/// [`cast_lifetime_write`](PtrWriteCastLifetimeExt::cast_lifetime_write)
+/// for pointers to references, to cast the lifetime of the reference type to
+/// something else before writing to the pointer (extremely, _extremely_
+/// simple to misuse!)
 pub(crate) trait PtrWriteCastLifetimeExt<T> {
 	/// Convenience method to change the lifetime of the reference type of the
 	/// pointer, then call `write` on the casted pointer
