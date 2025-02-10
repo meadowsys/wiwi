733a4ebcafbb5df95bf40a61c9a6819be7641217
Meadow Liu <meadowsys@kiwin.gay>
2025-02-09 18:10:47 -0800

    goodbye entire crate again
    lmao
    
    last night I had a realisation, followed by an idea, that means my entire fundamental API design of the types needs to change, so, I may as well rewrite everything? also built on macros from the ground up?

--- src/lib.rs
+++ src/lib.rs
@@ -26,49 +26,49 @@
 	)
 )]
 
-pub use self::any::ExternAny;
-pub use self::bigint::{ ExternBigint, ExternBigintNs, bigint };
-pub use self::object::ExternObject;
-pub use self::number::{ ExternNumber, ExternNumberNs, ExternNumberObject, number };
-#[doc(inline)]
-pub use self::reflect::{ ExternReflectNs, reflect };
-pub use self::string::{ ExternString, ExternStringNs, ExternStringObject };
-pub use wasm_bindgen::prelude::wasm_bindgen;
+// pub use self::any::ExternAny;
+// pub use self::bigint::{ ExternBigint, ExternBigintNs, bigint };
+// pub use self::object::ExternObject;
+// pub use self::number::{ ExternNumber, ExternNumberNs, ExternNumberObject, number };
+// #[doc(inline)]
+// pub use self::reflect::{ ExternReflectNs, reflect };
+// pub use self::string::{ ExternString, ExternStringNs, ExternStringObject };
+// pub use wasm_bindgen::prelude::wasm_bindgen;
 
-pub mod extern_crates;
-pub mod util;
+// pub mod extern_crates;
+// pub mod util;
 
-mod any;
-mod bigint;
-mod boolean;
-mod object;
-mod number;
-pub mod reflect;
-mod string;
-mod symbol;
+// mod any;
+// mod bigint;
+// mod boolean;
+// mod object;
+// mod number;
+// pub mod reflect;
+// mod string;
+// mod symbol;
 
-#[cfg(test)]
-wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+// #[cfg(test)]
+// wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
-#[allow(
-	dead_code,
-	unused_imports,
-	reason = "prelude"
-)]
-mod prelude_internal {
-	pub(crate) use crate::{ ExternAny, ExternObject, wasm_bindgen };
-	pub(crate) use crate::util::*;
-	pub(crate) use core::mem::{ ManuallyDrop, MaybeUninit, transmute };
-	pub(crate) use core::ops::Deref;
-}
+// #[allow(
+// 	dead_code,
+// 	unused_imports,
+// 	reason = "prelude"
+// )]
+// mod prelude_internal {
+// 	pub(crate) use crate::{ ExternAny, ExternObject, wasm_bindgen };
+// 	pub(crate) use crate::util::*;
+// 	pub(crate) use core::mem::{ ManuallyDrop, MaybeUninit, transmute };
+// 	pub(crate) use core::ops::Deref;
+// }
 
-#[allow(
-	dead_code,
-	unused_imports,
-	reason = "prelude"
-)]
-#[cfg(test)]
-mod prelude_test {
-	pub(crate) use wasm_bindgen_test::wasm_bindgen_test;
-	pub(crate) use core::hint::black_box;
-}
+// #[allow(
+// 	dead_code,
+// 	unused_imports,
+// 	reason = "prelude"
+// )]
+// #[cfg(test)]
+// mod prelude_test {
+// 	pub(crate) use wasm_bindgen_test::wasm_bindgen_test;
+// 	pub(crate) use core::hint::black_box;
+// }
