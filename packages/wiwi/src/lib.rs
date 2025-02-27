2ef4a5facd342b7d335e6c9453585769638c5d67
Meadow Liu <meadowsys@kiwin.gay>
2025-02-26 20:51:50 -0800

    I removed the changelog for now I forgor

--- packages/wiwi/src/lib.rs
+++ packages/wiwi/src/lib.rs
@@ -27,10 +27,6 @@
 #[cfg(target_pointer_width = "16")]
 compile_error!("16-bit platforms are not supported yet (but please do file an issue if for whatever reason you do need it, I would be happy to add support!)");
 
-#[cfg(any(doc, docsrs, kiwingay))]
-#[doc = include_str!("../CHANGELOG.md")]
-pub mod _changelog {}
-
 pub mod prelude;
 pub mod prelude_std;
 
