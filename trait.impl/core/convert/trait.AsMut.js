e30a9aa11596e8fb8b846941556d70dbed1f27bd
smol <107521333+a-tiny-kirin@users.noreply.github.com>
2025-03-14 10:48:16 +0000

    (automated) deploy from commit 935c90e3ac68dc9f9a09690b8dd9e35f9db0d0cf

--- trait.impl/core/convert/trait.AsMut.js
+++ trait.impl/core/convert/trait.AsMut.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["wiwi",[["impl&lt;T, U, F&gt; <a class=\"trait\" href=\"wiwi/prelude/trait.AsMut.html\" title=\"trait wiwi::prelude::AsMut\">AsMut</a>&lt;U&gt; for <a class=\"struct\" href=\"wiwi/lazy_wrap/struct.LazyWrap.html\" title=\"struct wiwi::lazy_wrap::LazyWrap\">LazyWrap</a>&lt;T, F&gt;<div class=\"where\">where\n    F: <a class=\"trait\" href=\"wiwi/prelude/trait.FnOnce.html\" title=\"trait wiwi::prelude::FnOnce\">FnOnce</a>() -&gt; T,\n    T: <a class=\"trait\" href=\"wiwi/prelude/trait.AsMut.html\" title=\"trait wiwi::prelude::AsMut\">AsMut</a>&lt;U&gt;,\n    U: ?<a class=\"trait\" href=\"wiwi/prelude/trait.Sized.html\" title=\"trait wiwi::prelude::Sized\">Sized</a>,</div>"]]],["wiwi_util",[]]]);
+    var implementors = Object.fromEntries([["wiwi",[["impl&lt;T, U, F&gt; <a class=\"trait\" href=\"wiwi/prelude/trait.AsMut.html\" title=\"trait wiwi::prelude::AsMut\">AsMut</a>&lt;U&gt; for <a class=\"struct\" href=\"wiwi/lazy_wrap/struct.LazyWrap.html\" title=\"struct wiwi::lazy_wrap::LazyWrap\">LazyWrap</a>&lt;T, F&gt;<div class=\"where\">where\n    F: <a class=\"trait\" href=\"wiwi/prelude/trait.FnOnce.html\" title=\"trait wiwi::prelude::FnOnce\">FnOnce</a>() -&gt; T,\n    T: <a class=\"trait\" href=\"wiwi/prelude/trait.AsMut.html\" title=\"trait wiwi::prelude::AsMut\">AsMut</a>&lt;U&gt;,\n    U: ?<a class=\"trait\" href=\"wiwi/prelude/trait.Sized.html\" title=\"trait wiwi::prelude::Sized\">Sized</a>,</div>"]]],["wiwi_chain",[["impl&lt;T, T2&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;T2&gt; for <a class=\"struct\" href=\"wiwi_chain/struct.Chain.html\" title=\"struct wiwi_chain::Chain\">Chain</a>&lt;T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;T2&gt;,</div>"]]],["wiwi_util",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[682,17]}
+//{"start":57,"fragment_lengths":[682,494,17]}
--- trait.impl/core/convert/trait.AsRef.js
+++ trait.impl/core/convert/trait.AsRef.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["wiwi",[["impl&lt;T, U, F&gt; <a class=\"trait\" href=\"wiwi/prelude/trait.AsRef.html\" title=\"trait wiwi::prelude::AsRef\">AsRef</a>&lt;U&gt; for <a class=\"struct\" href=\"wiwi/lazy_wrap/struct.LazyWrap.html\" title=\"struct wiwi::lazy_wrap::LazyWrap\">LazyWrap</a>&lt;T, F&gt;<div class=\"where\">where\n    F: <a class=\"trait\" href=\"wiwi/prelude/trait.FnOnce.html\" title=\"trait wiwi::prelude::FnOnce\">FnOnce</a>() -&gt; T,\n    T: <a class=\"trait\" href=\"wiwi/prelude/trait.AsRef.html\" title=\"trait wiwi::prelude::AsRef\">AsRef</a>&lt;U&gt;,\n    U: ?<a class=\"trait\" href=\"wiwi/prelude/trait.Sized.html\" title=\"trait wiwi::prelude::Sized\">Sized</a>,</div>"]]],["wiwi_util",[]]]);
+    var implementors = Object.fromEntries([["wiwi",[["impl&lt;T, U, F&gt; <a class=\"trait\" href=\"wiwi/prelude/trait.AsRef.html\" title=\"trait wiwi::prelude::AsRef\">AsRef</a>&lt;U&gt; for <a class=\"struct\" href=\"wiwi/lazy_wrap/struct.LazyWrap.html\" title=\"struct wiwi::lazy_wrap::LazyWrap\">LazyWrap</a>&lt;T, F&gt;<div class=\"where\">where\n    F: <a class=\"trait\" href=\"wiwi/prelude/trait.FnOnce.html\" title=\"trait wiwi::prelude::FnOnce\">FnOnce</a>() -&gt; T,\n    T: <a class=\"trait\" href=\"wiwi/prelude/trait.AsRef.html\" title=\"trait wiwi::prelude::AsRef\">AsRef</a>&lt;U&gt;,\n    U: ?<a class=\"trait\" href=\"wiwi/prelude/trait.Sized.html\" title=\"trait wiwi::prelude::Sized\">Sized</a>,</div>"]]],["wiwi_chain",[["impl&lt;T, T2&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;T2&gt; for <a class=\"struct\" href=\"wiwi_chain/struct.Chain.html\" title=\"struct wiwi_chain::Chain\">Chain</a>&lt;T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;T2&gt;,</div>"]]],["wiwi_util",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[682,17]}
+//{"start":57,"fragment_lengths":[682,494,17]}
