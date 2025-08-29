c28e24a977b6ad3dd90549719fa4ac9430b08a66
Meadow Liu <meadowsys@kiwin.gay>
2025-08-28 23:05:03 -0700

    felt cute nuked everything

---
+++ .old/wiwi/src/lib.rs
@@ -0,0 +1,60 @@
+#![no_implicit_prelude]
+
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+
+#![cfg_attr(all(docsrs, kiwingay), doc = "")]
+#![cfg_attr(
+	all(docsrs, kiwingay),
+	doc = concat!(
+		"These docs have been built from commit [",
+		env!("KIWINGAY_DEPLOY_COMMIT_SHORT"),
+		"](https://github.com/meadowsys/wiwi/commit/",
+		env!("KIWINGAY_DEPLOY_COMMIT"),
+		")."
+	)
+)]
+
+#![cfg_attr(
+	feature = "nightly",
+	feature(
+		const_trait_impl,
+		integer_atomics,
+		f16,
+		f128,
+		// used by `rc_nightly`
+		ptr_metadata
+	)
+)]
+
+#[cfg(target_pointer_width = "16")]
+compile_error!("16-bit platforms are not supported yet (but please do file an issue if for whatever reason you do need it, I would be happy to add support!)");
+
+#[doc(hidden)]
+pub extern crate wiwi_macro_proc as __internal_proc_macros;
+
+extern crate wiwi_util;
+pub use wiwi_util::*;
+
+// #[cfg(feature = "chain")]
+pub extern crate wiwi_chain as chain;
+
+pub mod aoc;
+pub mod builder;
+pub mod clock_timer;
+pub mod cron;
+// todo fix this lol
+// pub mod encoding;
+pub mod filetypes;
+pub mod lazy_wrap;
+pub mod lsl;
+pub mod macro_util;
+pub mod mem_use;
+pub mod nominal;
+pub mod num;
+pub mod parser;
+pub mod rc;
+#[cfg(feature = "nightly")]
+pub mod rc_nightly;
+pub mod string;
+pub mod tuple;
+pub mod vh;
