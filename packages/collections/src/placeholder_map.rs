f34c0e4b982f1bcf0684a128d93f8feef8a762a7
Meadow Liu <meadowsys@kiwin.gay>
2026-01-31 20:41:43 -0800

    few methods and struct stubs
    ~sf

--- packages/collections/src/placeholder_map.rs
+++ packages/collections/src/placeholder_map.rs
@@ -3,6 +3,7 @@
 use crate::DefaultHashBuilder;
 
 use allocator_api2::alloc::{ Allocator, Global };
+use core::cell::UnsafeCell;
 use hashbrown::{ HashMap, HashSet };
 use std::rc::Rc;
 
@@ -10,8 +11,8 @@ pub struct PlaceholderMap<K, V, S = DefaultHashBuilder, A = Global>
 where
 	A: Allocator
 {
-	keys: HashMap<K, Rc<V>, S, A>,
-	values: HashSet<Rc<V>, S, A>
+	keys: HashMap<K, Rc<UnsafeCell<V>>, S, A>,
+	values: HashSet<Rc<UnsafeCell<V>>, S, A>
 }
 
 impl<K, V> PlaceholderMap<K, V> {
@@ -160,6 +161,86 @@ where
 	}
 }
 
+impl<K, V, S, A> PlaceholderMap<K, V, S, A>
+where
+	A: Allocator
+{
+	#[inline]
+	pub fn key_allocator(&self) -> &A {
+		self.keys.allocator()
+	}
+
+	#[inline]
+	pub fn value_allocator(&self) -> &A {
+		self.values.allocator()
+	}
+
+	#[inline]
+	pub fn key_hasher(&self) -> &S {
+		self.keys.hasher()
+	}
+
+	#[inline]
+	pub fn value_hasher(&self) -> &S {
+		self.values.hasher()
+	}
+
+	#[inline]
+	pub fn key_capacity(&self) -> usize {
+		self.keys.capacity()
+	}
+
+	#[inline]
+	pub fn value_capacity(&self) -> usize {
+		self.values.capacity()
+	}
+
+	#[inline]
+	pub fn keys(&self) -> Keys<'_, K, V> {
+		Keys { inner: self.keys.keys() }
+	}
+
+	#[inline]
+	pub fn values(&self) -> Values<'_, V> {
+		Values { inner: self.values.iter() }
+	}
+
+	// todo
+	// pub fn values_mut(&mut self) -> ValuesMut<'_, V> {}
+
+	#[inline]
+	pub fn iter(&self) -> Iter<'_, K, V> {
+		Iter { inner: self.keys.iter() }
+	}
+
+	// todo
+	// pub fn iter_mut(&self) -> Iter<'_, K, V> {}
+
+	#[inline]
+	pub fn len(&self) -> usize {
+		self.keys.len()
+	}
+
+	#[inline]
+	pub fn values_len(&self) -> usize {
+		self.values.len()
+	}
+
+	#[inline]
+	pub fn is_empty(&self) -> bool {
+		debug_assert_eq!(
+			self.keys.is_empty(),
+			self.values.is_empty(),
+			"failure to keep keys and values in sync"
+		);
+
+		self.keys.is_empty()
+	}
+
+	// todo
+	// pub fn drain(&mut self) -> Drain<'_, K, V, A> {}
+}
+
 impl<K, V> Default for PlaceholderMap<K, V> {
 	#[inline]
 	fn default() -> Self {
@@ -186,3 +267,23 @@ where
 // 	S: Sync,
 // 	A: Allocator + Sync
 // {}
+
+// todo thread safety traits
+pub struct Keys<'h, K, V> {
+	inner: hashbrown::hash_map::Keys<'h, K, Rc<UnsafeCell<V>>>
+}
+
+// todo thread safety traits
+pub struct Values<'h, V> {
+	inner: hashbrown::hash_set::Iter<'h, Rc<UnsafeCell<V>>>
+}
+
+// todo
+// todo thread safety traits
+// pub struct ValuesMut<'h, V> {
+// 	inner: hashbrown::hash_set::IntoIter<V>
+// }
+
+pub struct Iter<'h, K, V> {
+	inner: hashbrown::hash_map::Iter<'h, K, Rc<UnsafeCell<V>>>
+}
