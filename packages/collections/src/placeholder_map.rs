295da2d9993c9702d838c885a3a70915d7bbde46
Meadow Liu <meadowsys@kiwin.gay>
2026-02-02 03:02:30 -0800

    impl Debug
    ~sf

--- packages/collections/src/placeholder_map.rs
+++ packages/collections/src/placeholder_map.rs
@@ -5,6 +5,7 @@ use crate::DefaultHashBuilder;
 use self::rc_mut::RcMut;
 
 use allocator_api2::alloc::{ Allocator, Global };
+use core::fmt::{ self, Debug };
 use core::hash::{ BuildHasher, Hash, Hasher };
 use core::iter::FusedIterator;
 use hashbrown::HashMap;
@@ -550,6 +551,18 @@ where
 			// after the first insertion of any given v
 			self.insert_value_ref(k.clone(), v);
 		});
+	}
+}
+
+impl<K, V, S, A> fmt::Debug for PlaceholderMap<K, V, S, A>
+where
+	K: Debug,
+	V: Debug,
+	A: Allocator
+{
+	#[inline]
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		f.debug_map().entries(self.iter()).finish()
 	}
 }
 
