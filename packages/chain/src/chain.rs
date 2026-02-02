00f812c760ff63bbbee9d9f69faab419e638a05f
Meadow Liu <meadowsys@kiwin.gay>
2026-02-02 03:48:15 -0800

    put back the scaffolds of a chaining API
    ~sf

---
+++ packages/chain/src/chain.rs
@@ -0,0 +1,68 @@
+#[must_use = "a chain takes ownership of itself, performs the action, then returns itself again"]
+pub struct Chain<T> {
+	inner: T
+}
+
+impl<T> Chain<T> {
+	#[inline]
+	pub fn from_inner(inner: T) -> Self {
+		Self { inner }
+	}
+
+	#[inline]
+	pub fn into_inner(self) -> T {
+		self.inner
+	}
+
+	#[inline]
+	pub fn as_inner(&self) -> &T {
+		&self.inner
+	}
+
+	#[inline]
+	pub fn as_inner_mut(&mut self) -> &mut T {
+		&mut self.inner
+	}
+
+	#[inline]
+	pub fn as_inner_chain(&self) -> Chain<&T> {
+		self.as_inner().into_chain()
+	}
+
+	#[inline]
+	pub fn as_inner_mut_chain(&mut self) -> Chain<&mut T> {
+		self.as_inner_mut().into_chain()
+	}
+
+	#[inline]
+	pub fn with_inner<F>(self, f: F) -> Self
+	where
+		F: FnOnce(&T)
+	{
+		f(self.as_inner());
+		self
+	}
+
+	#[inline]
+	pub fn with_inner_mut<F>(mut self, f: F) -> Self
+	where
+		F: FnOnce(&mut T)
+	{
+		f(self.as_inner_mut());
+		self
+	}
+}
+
+pub trait ChainInner: Sized {
+	#[inline]
+	fn from_chain(chain: Chain<Self>) -> Self {
+		chain.into_inner()
+	}
+
+	#[inline]
+	fn into_chain(self) -> Chain<Self> {
+		Chain::from_inner(self)
+	}
+}
+
+impl<T> ChainInner for T {}
---
+++ packages/chain/src/lib.rs
@@ -0,0 +1,3 @@
+pub use chain::{ Chain, ChainInner };
+
+mod chain;
