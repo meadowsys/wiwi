5155dfc6b670256998c6c8a9283aa21e3e678b98
Meadow Liu <meadowsys@kiwin.gay>
2026-02-03 18:42:57 -0800

    add a few vec chain fns
    need to figure out how to make autoderef work in a neat way, cause right now so much functionality is not able to be implemented with the macro because no autoderef
    
    ~sf

--- packages/chain/src/vec.rs
+++ packages/chain/src/vec.rs
@@ -2,6 +2,29 @@ use crate::prelude_internal::*;
 
 #[chain_fn]
 impl<T> Chain<Vec<T>> {
+	#[chain_doc("Vec::new")]
 	pub fn new() -> Self {}
+
+	#[chain_doc("Vec::with_capacity")]
+	pub fn with_capacity(capacity: usize) -> Self {}
+
+	#[chain_doc("Vec::from_raw_parts")]
+	pub unsafe fn from_raw_parts(ptr: *mut T, length: usize, capacity: usize) -> Self {}
+
+	pub fn append(&mut self, other: &mut Vec<T>) {}
+	// pub fn binary_search(&self, x: &T) -> Result<usize, usize>
+	// where
+	// 	T: Ord
+	// {}
+	pub fn capacity(&self) -> usize {}
+	pub fn clear(&mut self) {}
+	// pub fn clone_from_slice(&mut self, src: &[T])
+	// where
+	// 	T: Clone
+	// {}
+	// pub fn copy_from_slice(&mut self, src: &[T])
+	// where
+	// 	T: Copy
+	// {}
 	pub fn push(&mut self, value: T) {}
 }
